1f9eff6be6f2b3351f07763819af7e338d2665ca
Thando-cyber <evogeneration345@gmail.com>
2022-06-22 15:13:56 +0200

    final

---
+++ M2 Snapshots/DataSet2.cs
@@ -0,0 +1,54 @@
+﻿using M2_Snapshots.DataSet2TableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M2_Snapshots
+{
+
+
+    partial class DataSet2
+
+    {
+
+    }
+}
+
+namespace M2_Snapshots.DataSet2TableAdapters {
+
+
+    public partial class DataTable1TableAdapter
+    {
+        public string userID ="22010";
+        public string txt;
+
+        public string textS
+        {
+            get { return txt; }
+            set { txt = value; }
+        }
+
+        private void DataTable1TableAdapter_Load(object sender, EventArgs e)
+        {
+            this.InitCommandCollection();
+            userID = txt;
+        }
+
+        private void InitCommandCollection()
+        {
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[1];
+            this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[0].Connection = this.Connection;
+            this._commandCollection[0].CommandText = "SELECT student.*, subjectResults.*, subjects.*\r\nFROM   student INNER JOIN\r\n      " +
+                "       subjectResults ON student.stu_ID = subjectResults.studentID INNER JOIN\r\n " +
+                "            subjects ON subjectResults.subjectID = subjects.subject_ID where stu_ID="+int.Parse(userID);
+            this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+        }
+    }
+}
